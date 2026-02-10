df3a1c7e8dc690d6cf0235a7d1cd0cd7a8f4952c
João Fonseca <jcsf2020@gmail.com>
2026-02-10 01:54:23 +0000

    dq: fix not_null check to use customer_nk in dim_customer

--- sql/data_quality/dim_customer/not_null_customer_id.sql
+++ sql/data_quality/dim_customer/not_null_customer_id.sql
@@ -2,7 +2,7 @@
 -- Contract:
 -- - 0 rows => PASS
 -- - 1+ rows => FAIL
-select customer_id
+select customer_nk
 from analytics.dim_customer
-where customer_id is null
+where customer_nk is null
 limit 1;
